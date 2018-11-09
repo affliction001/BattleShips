1c4d6c75e8e5058802f1babb66c81b900936bd36
Vladislav Semenov <affliction001@mail.ru>
2018-11-09 21:51:55 +0300

    Попробовал сделать набросок для создания двух квадратов и отрисовки их в канвасе. Пока не очень успешно. Не могу придумать хорошую абстракцию для проектирования.

--- script/test.js
+++ script/test.js
@@ -1,13 +1,39 @@
 'use strict';
 
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
+const STEP = 100;
 
-context.beginPath();
-context.moveTo(100, 100);
-context.lineTo(100, 200);
-context.lineTo(200, 200);
-context.lineTo(200, 100);
-context.closePath();
-context.fillStyle = '#f00';
-context.fill();
+class Box {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  setBoxCoordinate(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  moveRight() {
+    this.x += STEP;
+  }
+}
+
+class View {
+  constructor() {
+    this.context = document.getElementById('canvas').getContext('2d');
+  }
+
+  drawShip(ship) {
+    this.context.fillRect(ship.x, ship.y, ship.width, ship.height);
+  }
+}
+
+const box_1 = new Box(100, 100);
+box_1.setBoxCoordinate(100, 0);
+
+const box_2 = new Box(100, 100);
+box_2.setBoxCoordinate(400, 0);
+
+const view = new View();
+view.drawShip(box_1);
+view.drawShip(box_2);
