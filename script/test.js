4226f5f7ee33693ed07ce57ffbe97c2b6fd111cc
Vladislav Semenov <affliction001@mail.ru>
2018-11-10 18:54:23 +0300

    Переписал все еще раз

--- script/test.js
+++ script/test.js
@@ -1,39 +1,83 @@
 'use strict';
 
-const STEP = 100;
+const SHIPS = [];
 
-class Box {
+class Ship {
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.coordinateX = 0;
+    this.coordinateY = 0;
   }
 
-  setBoxCoordinate(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-
-  moveRight() {
-    this.x += STEP;
+  setLocation(X, Y) {
+    this.coordinateX = X;
+    this.coordinateY = Y;
   }
 }
 
-class View {
+class Display {
   constructor() {
     this.context = document.getElementById('canvas').getContext('2d');
   }
 
-  drawShip(ship) {
-    this.context.fillRect(ship.x, ship.y, ship.width, ship.height);
+  redrawScreen(ships) {
+    this.context.clearRect(0, 0, 500, 500);
+    ships.forEach(ship => {
+      this.context.fillRect(ship.coordinateX, ship.coordinateY, ship.width, ship.height);
+    });
   }
 }
 
-const box_1 = new Box(100, 100);
-box_1.setBoxCoordinate(100, 0);
+class Controller {
+  constructor() {}
 
-const box_2 = new Box(100, 100);
-box_2.setBoxCoordinate(400, 0);
+  createShips() {
+    const ship_11 = new Ship(50, 50);
+    const ship_12 = new Ship(50, 50);
+    const ship_13 = new Ship(50, 50);
+    const ship_14 = new Ship(50, 50);
+    const ship_21 = new Ship(100, 50);
+    const ship_22 = new Ship(100, 50);
+    const ship_23 = new Ship(100, 50);
+    const ship_31 = new Ship(150, 50);
+    const ship_32 = new Ship(150, 50);
+    const ship_41 = new Ship(200, 50);
+    SHIPS.push(ship_11);
+    SHIPS.push(ship_12);
+    SHIPS.push(ship_13);
+    SHIPS.push(ship_14);
+    SHIPS.push(ship_21);
+    SHIPS.push(ship_22);
+    SHIPS.push(ship_23);
+    SHIPS.push(ship_31);
+    SHIPS.push(ship_32);
+    SHIPS.push(ship_41);
+  }
+}
 
-const view = new View();
-view.drawShip(box_1);
-view.drawShip(box_2);
+function main() {
+  const controller = new Controller();
+  controller.createShips();
+
+  console.log(SHIPS);
+
+  /*document.querySelector('body').addEventListener('keydown', event => {
+    if (event.keyCode === 40) {
+      console.log('down');
+    }
+
+    if (event.keyCode === 37) {
+      console.log('left');
+    }
+
+    if (event.keyCode === 38) {
+      console.log('up');
+    }
+
+    if (event.keyCode === 39) {
+      console.log('right');
+    }
+  });*/
+}
+main();
